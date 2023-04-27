1b9ff18e5930125121eb40859e6e0f5d196e081d
Antonia Runge <antonia.runge@inwt-statistics.de>
2023-04-27 17:36:02 +0200

    add list of ids

--- R/01-getMappingIds.R
+++ R/01-getMappingIds.R
@@ -2,6 +2,10 @@ getMappingIds <- function() {
   sendQueryCache("mappings")$dbmappings
 }
 
+getMappingIdsList <- function(mappingId = "IsoMemo"){
+  Result(list(), mappingIds = as.list(getMappingIds()))
+}
+
 getMappingAll <- function() {
   Result(list(), mapping = sendQueryCache("mappingsAll"))
 }
