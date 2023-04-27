10e39fba8b275dc9ec1c316ee098055c0e63b567
Antonia Runge <antonia.runge@inwt-statistics.de>
2023-04-27 15:10:17 +0200

    test print in tests

--- R/01-getFields.R
+++ R/01-getFields.R
@@ -1,7 +1,7 @@
 getFields <- function(mappingId = "IsoMemo") {
   # only accepts a single mappingId
   data <- sendQueryCache("fields", mappingId = mappingId)
-
+print(paste("getFields: mappingId=", mappingId, " data:", data))
   c(
     names(data[[1]]),
     data[[2]]$variable,
