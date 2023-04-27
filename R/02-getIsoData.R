b51c55deb28b5b7daf333fa2aec72780d2359d29
Antonia Runge <antonia.runge@inwt-statistics.de>
2023-04-27 20:35:06 +0200

    update query

--- R/02-getIsoData.R
+++ R/02-getIsoData.R
@@ -1,5 +1,5 @@
 getIsoData <- function(x, dbsource, category, field, mappingId = "IsoMemo") {
-  print(paste(dbsource, "---", mappingId))
+  print(paste(collapse(dbsource), "---", mappingId))
   res <- sendQueryCache("isoData", dbsource = dbsource, mappingId = mappingId)
   print(res)
   data <- res[[1]]
