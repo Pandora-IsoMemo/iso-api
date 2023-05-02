cf3ac591af54b44218c65c670046be256cd6805d
Jan A <jan.abel@inwt-statistics.de>
2023-05-02 17:07:57 +0200

    fix namespace warnings

--- R/00-NAMESPACE.R
+++ R/00-NAMESPACE.R
@@ -1,5 +1,7 @@
 #' @importFrom dplyr left_join select %>% filter pull
+#' @importFrom httpuv runServer
 #' @importFrom modules module
+#' @importFrom plumber plumber
 #' @importFrom rlang "!!" .data
 #' @importFrom tidyr spread
 NULL
