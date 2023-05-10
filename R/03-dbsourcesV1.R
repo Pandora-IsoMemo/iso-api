b31f6d23c0651ffb9b723233772dc11b3e8307d7
Antonia Runge <antonia.runge@inwt-statistics.de>
2023-05-10 19:53:47 +0200

    try add is.Error

--- R/03-dbsourcesV1.R
+++ R/03-dbsourcesV1.R
@@ -1,13 +1,16 @@
 #' Backend V1 of the dbsources API
 #'
-#' @param mappingId optional id to select one of the mappings, default is "IsoMemo"
+#' @param mappingId (character) optional id to select one of the mappings, default is "IsoMemo"
+#'
 #' @export
 dbsourcesV1 <- function(mappingId) {
+
   mappingId <- mappingIdSanitize(mappingId, default = "IsoMemo")
+  res <- step(list(), validateMappingId, mappingId)
+  if (is.Error(res)) return(res)
 
   logging("Got request on v1/dbsources with: mappingId='%s'", collapse(mappingId))
 
-  res <- step(list(), validateMappingId, mappingId)
   # only if validation succeeded getDbsourceList() will be executed:
   res <- step(res, getDbsourceList, mappingId)
   res
--- R/03-mappingV1.R
+++ R/03-mappingV1.R
@@ -1,13 +1,16 @@
 #' Backend V1 of the mapping API
 #'
-#' @param mappingId optional id to select one of the mappings, default is "IsoMemo"
+#' @param mappingId (character) optional id to select one of the mappings, default is "IsoMemo"
+#'
 #' @export
 mappingV1 <- function(mappingId) {
+
   mappingId <- mappingIdSanitize(mappingId, default = "IsoMemo")
-  ## Version 1 of the API
+  res <- step(list(), validateMappingId, mappingId)
+  if (is.Error(res)) return(res)
+
   logging("Got request on v1/mapping with: mappingId='%s'", collapse(mappingId))
 
-  res <- step(list(), validateMappingId, mappingId)
   # only if validation succeeded getMapping() will be executed:
   res <- step(res, getMapping, mappingId)
   res
