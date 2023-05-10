cfab4c9bda57a88ac61324ed4c8400edfe1949fc
Antonia Runge <antonia.runge@inwt-statistics.de>
2023-05-10 07:59:28 +0200

    Feat/new param to api specs (#7)
    * add new param to APISpecs
    
    * update readme

--- R/03-isoDataV1.R
+++ R/03-isoDataV1.R
@@ -1,12 +1,12 @@
 #' Backend V1 of the API
 #'
+#' @param mappingId id to select one of the mappings, e.g. "IsoMemo"
 #' @param dbsource (character) filter for database proider
 #' @param category (character) filter for field category
 #' @param field (character) filter for fields
-#' @param mappingId id to select one of the mappings, e.g. "IsoMemo"
 #'
 #' @export
-isoDataV1 <- function(dbsource, category, field, mappingId = "IsoMemo") {
+isoDataV1 <- function(mappingId = "IsoMemo", dbsource, category, field) {
 
   mappingId <- mappingIdSanitize(mappingId, default = "IsoMemo")
   res <- step(list(), validateMappingId, mappingId)
