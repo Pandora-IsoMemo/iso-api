134b6ea04d1145c4c0b17159cfd313e5fefb4e0a
Antonia Runge <antonia.runge@inwt-statistics.de>
2023-06-07 17:50:53 +0200

    update namespace

--- R/00-NAMESPACE.R
+++ R/00-NAMESPACE.R
@@ -1,7 +1,5 @@
 #' @importFrom dplyr left_join select %>% filter pull
-#' @importFrom httpuv runServer
 #' @importFrom modules module
-#' @importFrom plumber plumber
 #' @importFrom rlang "!!" .data
 #' @importFrom tidyr spread
 NULL
