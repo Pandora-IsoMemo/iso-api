4e9a8e4b6120b0b90ee245958efe9c9eb33b31bb
Antonia Runge <antonia.runge@inwt-statistics.de>
2023-10-12 21:01:27 +0200

    try new path

--- tests/testthat/test-compareDatabases.R
+++ tests/testthat/test-compareDatabases.R
@@ -42,9 +42,8 @@ testthat::test_that("Compare tables from main and test databases", {
   }
 
   # Arrange:
-  if (file.exists(file.path(system.file(file.path("..", ".."), package = "MpiIsoApi"), ".inwt", "MpiIsoApi", "mpi-iso-api-config.R"))) {
-    print(list.files(system.file(file.path("..", ".."), package = "MpiIsoApi")) %>% paste0(collapse = ", "))
-    source(file.path(system.file(file.path("..", ".."), package = "MpiIsoApi"), ".inwt", "MpiIsoApi", "mpi-iso-api-config.R"), settings)
+  if (file.exists(file.path(system.file(file.path(".."), package = "MpiIsoApi"), ".inwt", "MpiIsoApi", "mpi-iso-api-config.R"))) {
+    source(file.path(system.file(file.path(".."), package = "MpiIsoApi"), ".inwt", "MpiIsoApi", "mpi-iso-api-config.R"), settings)
   } else
     source(file.path(settings$homeDir, "mpi-iso-api-config.R"), settings)
   resMain <- sendQueryCache("isoData",
@@ -55,8 +54,8 @@ testthat::test_that("Compare tables from main and test databases", {
   extraNumericMain <- spread(resMain[[2]], .data$variable, .data$value)
   extraCharacterMain <- spread(resMain[[3]], .data$variable, .data$value)
 
-  if (file.exists(file.path(system.file(file.path("..", ".."), package = "MpiIsoApi"), ".inwt", "MpiIsoApi", "mpi-iso-api-test-config.R")))
-    source(file.path(system.file(file.path("..", ".."), package = "MpiIsoApi"), ".inwt", "MpiIsoApi", "mpi-iso-api-test-config.R"), settings)
+  if (file.exists(file.path(system.file(file.path(".."), package = "MpiIsoApi"), ".inwt", "MpiIsoApi", "mpi-iso-api-test-config.R")))
+    source(file.path(system.file(file.path(".."), package = "MpiIsoApi"), ".inwt", "MpiIsoApi", "mpi-iso-api-test-config.R"), settings)
   else
     source(file.path(settings$homeDir, "mpi-iso-api-test-config.R"), settings)
   resTest <- sendQueryCache("isoData",
